82e2ffdcd58d69771d74eab3f69143d7ac9768e8
unknown <abhishekmaurya9f@gmail.com>
2021-07-13 19:35:04 +0530

    First Commit

--- js/script.js
+++ js/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Discord Bot Developer"],
+        strings: ["Web Developer", "Discord Bot Developer", "AI Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Discord Bot Developer"],
+        strings: ["Web Developer", "Discord Bot Developer", "AI Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
