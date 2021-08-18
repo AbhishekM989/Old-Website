ebc1b5c01753ee8516f46dedf3be8a2dfa8e2d6a
Abhishek <abhishekmaurya9f@gmail.com>
2021-08-18 22:53:06 +0530

    Minor Changes

--- js/script.js
+++ js/script.js
@@ -77,7 +77,7 @@ function setNavigation() {
     path = path.replace(/\/$/, "");
     path = decodeURIComponent(path);
 
-    $(".nav a").each(function () {
+    $(".menu a").each(function () {
         var href = $(this).attr('href');
         if (path.substring(0, href.length) === href) {
             $(this).closest('li').addClass('active');
