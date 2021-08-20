598341fe4f3c81041bfe5d3d6ef52b4152bd1c78
Abhishek <abhishekmaurya9f@gmail.com>
2021-08-20 19:40:07 +0530

    Minor Changes

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Discord Bot Developer", "AI Developer"],
+        strings: ["Web Developer", "Discord Bot Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
